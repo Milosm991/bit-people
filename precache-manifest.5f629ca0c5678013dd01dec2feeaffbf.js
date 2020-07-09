b549b45e47709d9a3e5005a78343993ff12d471d
Milosm991 <milosmanojlovic91@gmail.com>
2020-07-09 17:53:48 +0200

    Updates

---
+++ precache-manifest.5f629ca0c5678013dd01dec2feeaffbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545142b7562b4325905afc5185fe262c",
+    "url": "/bit-people/index.html"
+  },
+  {
+    "revision": "46a4b836e56f1a749b8a",
+    "url": "/bit-people/static/css/main.f9c186e2.chunk.css"
+  },
+  {
+    "revision": "d20b82785a8ee52f2b5e",
+    "url": "/bit-people/static/js/2.29d51848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bit-people/static/js/2.29d51848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a4b836e56f1a749b8a",
+    "url": "/bit-people/static/js/main.21bce1c3.chunk.js"
+  },
+  {
+    "revision": "e0827626558f33879c68",
+    "url": "/bit-people/static/js/runtime-main.500e00d3.js"
+  },
+  {
+    "revision": "6cfcc6be4fcc2243d131f5baa514cb94",
+    "url": "/bit-people/static/media/not-found.6cfcc6be.png"
+  }
+]);
